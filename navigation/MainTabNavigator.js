88a52a8e1ef2878c66739cfe36aed1f64f748059
Inseok <inseok.ucla@gmail.com>
2019-03-29 23:17:14 -0700

    Added lessonScreen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,6 +6,7 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import LessonsScreen from '../screens/LessonsScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -53,8 +54,23 @@ SettingsStack.navigationOptions = {
   ),
 };
 
+const LessonsStack = createStackNavigator({
+  Lessons: LessonsScreen,
+});
+
+LessonsStack.navigationOptions = {
+  tabBarLabel: 'Lessons',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-calendar' : 'md-calendar'}
+    />
+  ),
+};
+
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+  LessonsStack,
 });
